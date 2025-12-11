b82296df70809ce669392bca782d8d792c140e1e
Andrew Tate <tate.andrewjames@gmail.com>
2025-12-11 06:49:28 -0500

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,13 +1,15 @@
 """Utility functions for multimodal agents."""
 
 from src.utils.audio import WhisperTranscriber
-from src.utils.image import resize_frame, frames_to_grid
+from src.utils.image import resize_frame, frames_to_grid, FrameSaver, save_frame
 from src.utils.benchmark import BenchmarkRunner, BenchmarkResult
 
 __all__ = [
     "WhisperTranscriber",
     "resize_frame",
     "frames_to_grid",
+    "FrameSaver",
+    "save_frame",
     "BenchmarkRunner",
     "BenchmarkResult",
 ]
